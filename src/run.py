8f4c6ee349f9eb38d731ac90a1b567efa766bc42
chmielny <michal.chmielo@gmail.com>
2018-05-28 22:23:49 +0200

    poprawki

--- src/run.py
+++ src/run.py
@@ -4,7 +4,7 @@ import sys
 
 min_result =  math.inf
 for x in range(1, 100):
-	result = subprocess.run(['./a.out', sys.argv[1], '1', '96', '0'], stdout=subprocess.PIPE)
+	result = subprocess.run(['./a.out', sys.argv[1], sys.argv[2], sys.argv[3], '0'], stdout=subprocess.PIPE)
 	if min_result > int(result.stdout):
 		min_result = int(result.stdout)
 print( min_result )
